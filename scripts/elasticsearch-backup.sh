fceb1906735c7a7964d6d804259698bc1ae6c5ed
TheConnMan <bcconn2112@gmail.com>
2016-07-08 01:32:21 +0000

    Add ElasticSearch backup script

---
+++ scripts/elasticsearch-backup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Crontab config
+# 0 1 * * * /path/to/elasticsearch-backup.sh
+
+filename=`date +%H-%M-%S`-elasticsearch.zip
+zip -rq $filename /opt/data/elasticsearch/
+location=s3://augurworks-logs/`date +%Y/%m/%d/`
+/usr/local/bin/aws s3 cp $filename $location
+rm $filename
